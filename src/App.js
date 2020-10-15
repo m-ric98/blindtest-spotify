d3d87bea53b608fc320faddf34ce6df0ebb3964e
Aymeric <aymeric.degroote@telecom-paris.fr>
2020-10-15 17:47:35 +0200

    changed host to m-ric98.github.io

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
         <h1 className="App-title">Bienvenue sur le Blindtest</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai Blindtest ! Ok c'est bon </p>
+        <p>Il va falloir modifier le code pour faire un vrai Blindtest ! Wala </p>
       </div>
       <div className="App-buttons">
       </div>
